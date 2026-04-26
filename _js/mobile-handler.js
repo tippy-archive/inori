954067f932afe929dfc50d89aae86e4c793c36e9
tippy-archive <tippy-archive@gmail.com>
2026-04-26 20:36:48 +0900

    inori

--- _js/mobile-handler.js
+++ _js/mobile-handler.js
@@ -23,11 +23,11 @@
     const loaderImage = document.createElement('img');
     loaderImage.id = 'loader-image';
     loaderImage.style.cssText = `
-        width: 70vw;
+        width: 80vw;
         border-radius: 12px;
-        margin-bottom: 40px;
+        margin-bottom: 30px;
         object-fit: cover;
-        box-shadow: 0 4px 10px rgba(0,0,0,0.1);
+        box-shadow: 0 4px 10px rgba(0,0,0,0.3);
     `;
 
     const randomIndex = Math.floor(Math.random() * imageUrls.length);
