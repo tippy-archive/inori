d46a24f56b27bcd659439271c0c3390c947c2cbe
tippy-archive <tippy-archive@gmail.com>
2026-05-09 00:08:37 +0900

    inori

--- _js/mobile-handler.js
+++ _js/mobile-handler.js
@@ -5,10 +5,7 @@
         'https://lh3.googleusercontent.com/pw/AP1GczPbKbdOKBVLKAO8vg8kF8pYIs71abAerR2oy2bo1sDe1gvXpLD3xaM_9aQnqwIEJXeUdGjSSj-49cEZAWpIFMaSoS8BKOr0Aah7S_V-k6GniTp7B5r3Oftfn5mmQ1ykGRv-pSaOgjREkzejW0Y3j62M=w800',
         'https://lh3.googleusercontent.com/pw/AP1GczOC7hcxdZ30R8YOVzu7k7tm3kvQ7SxyyKAtFGVpkAKEXAIczhiQCyjsu3mzEJymxQFJBc7fYmyYxdhO_CSG9Tstgh2r9fGHNns3j9GHSt6gLOcWYT-7oXckmgeHFDKf7xwlcUKxkePrNJ7VO4mN5Bm4=w800',
         'https://lh3.googleusercontent.com/pw/AP1GczOmkU77pKA3zpkvGQCL5vNfU2cmX8SL0ZtyT7UhfaEeJXGVYSO3WP1Ge6gESX8AJAPbTKG2MCrRVoxdN2dU-DyPfcoYB3G_UO_BSPng0Jmx4J84hQNlo8Zo4Bh7enxSyuwb8QJQEW5vz07ZnFNSXt5t=w800',
-        'https://lh3.googleusercontent.com/pw/AP1GczNQos9IDXJDPV9lw6pb8yivCi-3ijK9U4ZrnTMMxrnruY9yszPC-froITVlNzvp-mFqc7psVEwnCe7K6DDn84Z2enbhZmPb0basL9AWQegzWG_EkI3tTiJYz8FfbyggJIR1nr2_-3VTPUxsneTBD8Tz=w800',
-        'https://lh3.googleusercontent.com/pw/AP1GczOHEJxJA24WpB48gZoR1YwhdcEYaGyHRUEehIZZaPJA07XHhKKwH22DmlhCUkxwRw-Y7HZ3LpgKV5e2GzVFZ1gFjZu6wRoFV180UdG7MCDtPDeG45nQ0cCy1VCqqYk0mbA3D2MrGqiIjNBWu69FnsTK=w800',
-        'https://lh3.googleusercontent.com/pw/AP1GczPai5oazu_pO6bTcFwLTZ4Eb1bKhlWRGg1BHgTN0bOMUQhBplT-ViLPTPttdrfUa66FWf69AoEQljaA0FaCQrhHst_rHVmv0J13HzpY1IBEnKZsjBqeQLNKkyVnS0Gap-K07fV6zlfW5Vg6G10vq26r=w800',
-        'https://lh3.googleusercontent.com/pw/AP1GczOxR9mQfUeqKUxTLkyu5us0GlYf4yiIOcJEF_dPyA4Chyr9OHYuRM5LmDnOFTYEWNp6_t7s6jQaS0L46E3kjG7GgSvGZOAJKALhE53XKk9BrHaiKN1u8279GGZaqvAkqxGHa1nC6ynSv10cD2FqhBfk=w800'
+        'https://lh3.googleusercontent.com/pw/AP1GczOHEJxJA24WpB48gZoR1YwhdcEYaGyHRUEehIZZaPJA07XHhKKwH22DmlhCUkxwRw-Y7HZ3LpgKV5e2GzVFZ1gFjZu6wRoFV180UdG7MCDtPDeG45nQ0cCy1VCqqYk0mbA3D2MrGqiIjNBWu69FnsTK=w800'
     ];
 
     if (window.innerWidth > 480) return;
@@ -28,7 +25,7 @@
         border-radius: 12px;
         margin-bottom: 20px;
         object-fit: cover;
-        box-shadow: 0 4px 10px rgba(0,0,0,0.4);
+        box-shadow: 0 2px 10px rgba(0,0,0,0.8);
     `;
 
     const randomIndex = Math.floor(Math.random() * imageUrls.length);
